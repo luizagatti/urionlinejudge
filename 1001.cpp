37cacbf204754231ab7b3aeaa53d0655cc5ba431
Luiza Gatti <luiza.gatti@gmail.com>
2015-10-20 19:50:49 -0200

    1001.cpp
    uri online judge

---
+++ 1001.cpp
@@ -0,0 +1,19 @@
+/*
+Luiza de Almeida Gatti
+All rights reserved
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (void){
+    int a, b, x;
+    cin >> a;
+    cin >> b;
+
+    x = a+b;
+
+    cout <<"X"<<" = " << x << endl;
+
+    return 0; //çuçeço
+}
